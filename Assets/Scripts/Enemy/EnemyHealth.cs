528fb0e3f066988ac1fc3a607eb1bc7ee38bec0e
Hangil Chang <hangil2010@naver.com>
2025-05-28 16:49:58 +0900

    [Feat] 적 처치 시 재화 증가 기능 추가, Issue#3 해결
    Issue#3 해결 : 파괴 여부를 확인하는 bool값을 추가, 조건문에 체력이 0 이하인 경우와 파괴되었는지 여부까지 판단하게 함

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -6,13 +6,16 @@ public class Health : MonoBehaviour
 {
     [Header("Attributes")]
     [SerializeField] private int hitPoints = 2;
-
+    [SerializeField] private int coinAmount = 50;
+    private bool isDestoryed = false;
     public void TakeDamage(int dmg)
     {
         hitPoints -= dmg;
-        if (hitPoints <= 0)
+        if (hitPoints <= 0 && !isDestoryed)
         {
             EnemySpawner.OnEnemyDestoryed?.Invoke();
+            LevelManager.main.IncreaseCurrency(coinAmount);
+            isDestoryed = true;
             Destroy(gameObject);
         }
     }
