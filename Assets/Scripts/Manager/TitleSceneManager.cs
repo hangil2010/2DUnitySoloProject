cf081e436e87755d12c247bab0a4ef2228b918ea
Hangil Chang <hangil2010@naver.com>
2025-06-02 17:35:41 +0900

    [Feat] 타이틀 씬 페이드 아웃, 인 효과 추가

--- Assets/Scripts/Manager/TitleSceneManager.cs
+++ Assets/Scripts/Manager/TitleSceneManager.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class TitleSceneManager : MonoBehaviour
 {
+    public Animator transition;
+    public float transitionTime = 1f;
     public void ChangeScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(LoadLevel(sceneName));
+    }
+
+    IEnumerator LoadLevel(string _sceneName)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(_sceneName);
     }
 
     public void ExitGame()
