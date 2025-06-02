cb92b44dadb8bf7ddbc74c9d4788ec1db9e2dc5a
Hangil Chang <hangil2010@naver.com>
2025-06-02 15:40:36 +0900

    [Feat] 선택한 타워 하이라이트 기능 추가

--- Assets/Scripts/Manager/BuildManager.cs
+++ Assets/Scripts/Manager/BuildManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class BuildManager : MonoBehaviour
 {
@@ -9,9 +10,8 @@ public class BuildManager : MonoBehaviour
 
     [Header("References")]
     [SerializeField] private Tower[] towers;
-
     private int selectedTower = 0;
-
+    private Image selectedImage;
     private void Awake()
     {
         main = this;
@@ -25,5 +25,13 @@ public class BuildManager : MonoBehaviour
     public void SetSelectedTower(int _selectedTower)
     {
         selectedTower = _selectedTower;
+        // 현재 선택된 이미지가 없다 > 클릭한 버튼의 이미지 활성화
+        // 헌재 선택된 이미지가 있다 > 바꿔라라
+    }
+    public void SetSelectedImage(Image _selectedImage)
+    {
+        if (selectedImage != null) selectedImage.gameObject.SetActive(false);
+        selectedImage = _selectedImage;
+        selectedImage.gameObject.SetActive(true);
     }
 }
